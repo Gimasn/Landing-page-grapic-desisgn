63b40ef413fef27a1f2d30fadac4e59d5d34321c
Gimasn <164129701+Gimasn@users.noreply.github.com>
2024-05-23 09:13:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let navbar = document.getElementById('mncl');
+const tombol = document.getElementById('menu');
+
+tombol.addEventListener('click', function(){
+  
+  navbar.classList.toggle('active');
+  
+});
